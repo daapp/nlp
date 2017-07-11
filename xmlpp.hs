eaad21999ae66d9e3aeb7a0e506084db8539f603
Alexander Danilov <alexander.a.danilov@gmail.com>
2017-07-11 23:53:55 +0300

    Add description. Fix pretty print for stdin input data.

--- xmlpp.hs
+++ xmlpp.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+{- Pretty print XML file.
+-}
+
 import qualified Data.Text.Lazy     as T
 import qualified Data.Text.Lazy.IO  as TIO
 import           System.Environment (getArgs, getProgName)
@@ -14,7 +17,7 @@ main = do
   case length args of
     0 -> do content <- TIO.getContents
             case parseText def content of
-              Right doc -> TIO.putStrLn $ renderText def doc
+              Right doc -> TIO.putStrLn $ renderText def {rsPretty = True} doc
               Left e    -> hPutStrLn stderr $ "Error: " ++ show e
     1 -> do doc <- X.readFile def $ head args
             TIO.putStrLn $ renderText def {rsPretty = True} doc
